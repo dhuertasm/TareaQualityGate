33a228b0fc11a4c29fa71756aae97334674b33ac
dhuertasm <dhuertasm@uniandes.edu.co>
2024-11-01 23:26:50 -0500

    prueba 11

--- src/app.py
+++ src/app.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 def suma(x,y):
     return x + y
 
@@ -8,10 +10,19 @@ def valor_retornado():
     else:
         return "Adios mundo!"
 
-
 def retorar_valor():
     flag = True
     if flag:
         return "Hola mundo!"
     else:
         return "Adios mundo!"
+
+def execute_query(user_input):
+    # Esta consulta es vulnerable a SQL Injection
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    query = f"SELECT * FROM users WHERE username = '{user_input}'"
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
