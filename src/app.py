ee9cd38f684620fe7edaf12a7941304fd8d854b0
dhuertasm <dhuertasm@uniandes.edu.co>
2024-11-01 23:12:08 -0500

    prueba 10

--- src/app.py
+++ src/app.py
@@ -1,3 +1,17 @@
 def suma(x,y):
     return x + y
-    
+
+def valor_retornado():
+    flag = True
+    if flag:
+        return "Hola mundo!"
+    else:
+        return "Adios mundo!"
+
+
+def retorar_valor():
+    flag = True
+    if flag:
+        return "Hola mundo!"
+    else:
+        return "Adios mundo!"
---
+++ src/sapp.py
@@ -0,0 +1,33 @@
+def if_else(x):
+    if x * 2 == 10:
+        return x
+    if x * 3 < 20:
+        return x
+    if x * 4 > 20:
+        return x
+    if x * 5 == 25:
+        return x
+    if x * 6 == 64:
+        return x
+    if x * 10 == 100:
+        return x
+    return x
+
+def else_if(x):
+    if x * 2 == 10:
+        return x
+    if x * 3 < 20:
+        return x
+    if x * 4 > 20:
+        return x
+    if x * 5 == 25:
+        return x
+    if x * 6 == 64:
+        return x
+    if x * 10 == 100:
+        return x
+    return x
+
+def insert_index(i):
+    d = ["a", "b", "c", "d", "f"]
+    return d[i]
